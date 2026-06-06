95b206fb84e682b9119fa62d9377a294ab6a339f
volodymyrlogika <volodymyr.logika@gmail.com>
2026-06-06 16:29:50 +0300

    додали сторінку бронювання

--- booking/urls.py
+++ booking/urls.py
@@ -6,4 +6,6 @@ from booking  import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('apartments/<int:apartment_id>', views.apartment_page, name='apartment_page'),
+    path('booking/<int:apartment_id>', views.booking_page, name='booking_page'),
+    
 ]
--- booking/views.py
+++ booking/views.py
@@ -5,6 +5,13 @@ def home(request):
     apartments = Apartment.objects.all()
     return render(request, 'home.html', {'apartments': apartments})
 
+
 def apartment_page(request, apartment_id):
     apartment = Apartment.objects.get(id = apartment_id)
     return render(request, 'apartment_page.html', {'apartment': apartment})
+
+
+def booking_page(request, apartment_id):
+    apartment = Apartment.objects.get(id = apartment_id)
+    
+    return render(request, 'booking.html', {'apartment': apartment})
